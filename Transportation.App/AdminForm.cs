c09cc6ce8f85ef41ce4271a4816e8a282dbccf8b
TajbiurShahriorRimon <tajbiurshahriorrimon@gmail.com>
2021-04-11 01:08:40 +0600

    Rimon's Second. Made changes whenever a button is clicked the button red light dynamically changes

--- Transportation.App/AdminForm.cs
+++ Transportation.App/AdminForm.cs
@@ -24,13 +24,22 @@ namespace Transportation.App
         private void adminBtnHome_Click(object sender, EventArgs e)
         {
             //adminHome1.Height = adminBtnHome.Height;
+            this.pnlRed.Height = this.adminBtnHome.Height;
+            this.pnlRed.Top = this.adminBtnHome.Top;
             adminHome1.BringToFront();
         }
 
         private void btnSchedule_Click(object sender, EventArgs e)
         {
             //schedule1.Height = btnSchedule.Height;
+            this.pnlRed.Height = this.btnSchedule.Height;
+            this.pnlRed.Top = this.btnSchedule.Top;
             schedule1.BringToFront();
+        }
+
+        private void schedule1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
