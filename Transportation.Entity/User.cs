173ea73e7c75bafd13dfbf4f287c1b634cce108c
Asif <asifahmed1132@gmail.com>
2021-05-09 23:37:58 +0600

    Minor change on userControl form and User entity

--- Transportation.Entity/User.cs
+++ Transportation.Entity/User.cs
@@ -13,6 +13,7 @@ namespace Transportation.Entity
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
+        public string Password { get; set; }
         public string UserType { get; set; }
         public string Salary { get; set; }
         public string Status { get; set; }
