9fccb8dc657b065f66f8402437636e0908da522e
Asif <asifahmed1132@gmail.com>
2021-05-09 21:28:15 +0600

    Create, update user and some input fields validation has been completed on userControl form

---
+++ Transportation.Repository/LoginRepo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transportation.Entity;
+using Transportation.Data;
+
+namespace Transportation.Repository
+{
+    public class LoginRepo
+    {
+        public static bool Save(Login l)
+        {
+            var sql = $"INSERT INTO [login] VALUES('{l.Password}', '{l.UserId}')";
+            var row = DataAccess.ExecuteDmlQuery(sql);
+            return row == 1;
+        }
+
+        public static bool Update(Login l)
+        {
+            var sql = $"UPDATE [login] SET password = '{l.Password}' where user_id = '{l.UserId}';";
+            var row = DataAccess.ExecuteDmlQuery(sql);
+            return row == 1;
+        }
+    }
+
+    
+}
---
+++ Transportation.Validation/CustomValidations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transportation.Validation
+{
+    internal static class CustomValidations
+    {
+        internal static bool ValidName(string name)
+        {
+            name = name.Replace(" ", "");
+            name = name.Replace("-", "");
+            return name.All(char.IsLetter);
+        }
+    }
+}
