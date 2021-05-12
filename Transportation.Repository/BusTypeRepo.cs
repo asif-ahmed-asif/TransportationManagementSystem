79ff87ee502155cf6de984e41795895fcd66c978
TajbiurShahriorRimon <tajbiurshahriorrimon@gmail.com>
2021-05-12 17:35:56 +0600

    Bus form validation and insertion done.

---
+++ Transportation.Repository/BusTypeRepo.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using Transportation.Data;
+
+namespace Transportation.Repository
+{
+    public class BusTypeRepo
+    {
+        public static string GetBusTypeForBus(string busType)
+        {
+            var sqlQuery = "select top 1 * from bus_type where bus_type = '" +busType+"';";
+            var row = DataAccess.GetDataTable(sqlQuery);
+            string data = row.Rows[0][0].ToString();
+            return data;
+        }
+    }
+}
